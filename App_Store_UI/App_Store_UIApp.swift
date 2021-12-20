d9178aa108a3795b6feb7b52da29c953facb933a
Stanley Pan <imoment.developer@gmail.com>
2021-12-20 16:40:32 -0500

    Initial Commit

---
+++ App_Store_UI/App_Store_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_Store_UIApp.swift
+//  App_Store_UI
+//
+//  Created by Stanley Pan on 2021/12/20.
+//
+
+import SwiftUI
+
+@main
+struct App_Store_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App_Store_UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App_Store_UI
+//
+//  Created by Stanley Pan on 2021/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
